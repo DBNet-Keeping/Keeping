ad436625d44a1a684e75b6f74135747767999e10
chldbwls <chldbwls8643@naver.com>
2023-12-08 00:49:47 +0900

    signup modify

--- signup.php
+++ signup.php
@@ -1,4 +1,12 @@
-<link rel=stylesheet href='signup.css' type='text/css'>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel=stylesheet href='signup.css' type='text/css'>
+</head>
+<body>
 <form method="post" action="check_signup.php" class="signupForm">
     <h2>SIGN UP</h2>
     <div class="idForm">
@@ -17,3 +25,5 @@
       SIGN UP
     </button>
   </form>
+</body>
+</html>
