5661afb7befeae4f7c296e65b5b5baffa550e011
ji-min-choi <4metree@naver.com>
2023-12-10 21:52:25 +0900

    Correcting Navbar Errors

--- assets/navbar.js
+++ assets/navbar.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
-  // 네비게이션 바 버튼에 클릭 이벤트 리스너 추가
-  $("#home").addClass("active");
-  $(".side").click(function () {
-    // 클릭된 버튼의 부모(<li>)에 'active' 클래스 추가
-    $(this).addClass("active");
+  let last_s = document.URL.lastIndexOf("/");
+  let last_o = document.URL.lastIndexOf(".");
+  let file_name = document.URL.substring(last_s + 1, last_o);
 
-    // 클릭된 버튼의 형제(<li>)들에서 'active' 클래스 제거
-    $(this).parent().siblings().find(".side").removeClass("active");
-  });
+  $("#" + file_name).addClass("active");
+  $("#" + file_name)
+    .parent()
+    .siblings()
+    .find(".side")
+    .removeClass("active");
 });
