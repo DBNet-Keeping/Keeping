a27c464d983f6f744de61dd680dda8a7541085bb
ji-min-choi <4metree@naver.com>
2023-12-12 02:43:37 +0900

    modify navbar

--- navbar.php
+++ navbar.php
@@ -16,8 +16,8 @@
   <div class="sidebar">
     <ul class="bar-ul">
       <li class="home_li"><a class="side" href="home.php" id="home">HOME</a></li>
-      <li class="home_li"><a class="side" href="#" id="transaction">TRANSACTION</a></li>
-      <li class="home_li"><a class="side" href="#" id="category">CATEGORY</a></li>
+      <li class="home_li"><a class="side" href="transaction.php" id="transaction">TRANSACTION</a></li>
+      <li class="home_li"><a class="side" href="category.php" id="category">CATEGORY</a></li>
       <li class="home_li"><a class="side" href="subscription.php" id="subscription">SUBSCRIPTION</a></li>
       <li class="home_li"><a class="side" href="mypage.php" id="mypage">MY PAGE</a></li>
     </ul>
